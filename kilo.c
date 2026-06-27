8bb6d7307516ea53d95545cce7d404481b8e6b5e
Kuuroojinn <94182798+Kuuroojinn@users.noreply.github.com>
2026-06-27 17:39:08 +0200

    added syntax highlight to search result

--- kilo.c
+++ kilo.c
@@ -41,7 +41,8 @@ enum editorKey {
 
 enum editorHighlight {
 	HL_NORMAL = 0,
-	HL_NUMBER
+	HL_NUMBER,
+	HL_MATCH
 };
 
 
@@ -207,6 +208,7 @@ void editorUpdateSyntax(erow *row) {
 int editorSyntaxToColor (int hl) {
 	switch(hl) {
 		case HL_NUMBER: return 31;
+		case HL_MATCH: return 34;
 		default: return 37;
 	}
 }
@@ -472,6 +474,8 @@ void editorFindCallback(char *query, int key) {
 			E.cy = current;
 			E.cx = editorRowRxToCx(row,match - row->render);
 			E.rowoff = E.numrows;
+
+			memset(&row->hl[match - row->render], HL_MATCH,strlen(query));
 			break;
 		}
 	}
